69eb24283f621953baa11db291d04df54fdd8559
Irvine Ye <irvine.ye@outlook.com>
2022-11-10 05:38:24 +0000

    Initial implementation of Tic-tac-toe game

--- week5/tests.py
+++ week5/tests.py
@@ -12,7 +12,17 @@ class TestLogic(unittest.TestCase):
         ]
         self.assertEqual(logic.get_winner(board), 'X')
 
-    # TODO: Test all functions from logic.py!
+    def test_other_player(self):
+        player = 'X'
+        self.assertEqual(logic.other_player(player), 'O')
+
+    def test_check_line(self):
+        player = None
+        adj1 = 'O'
+        adj2 = 'X'
+        self.assertEqual(logic.check_line(player,adj1, adj2), 'D')
+
+    # TODO: Test all the other functions from logic.py!
 
 
 if __name__ == '__main__':
